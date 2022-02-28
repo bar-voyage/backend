f09ff9948b7ba43a9fa4ac8c1ddda3e3a3e91bb8
sreyanalla <sreyanalla@gwu.edu>
2022-02-28 13:22:02 -0500

    add name to db store in /register

--- controllers/register_user.controller.js
+++ controllers/register_user.controller.js
@@ -3,9 +3,11 @@ const { postRegisterUserService } = require('../services/register_user.service')
 const postRegisterUser = async (req, res, next) => {
     email = req.body.email;
     password = req.body.password;
-    
+    fname = req.body.fname;
+    lname = req.body.lname;
+    console.log(req.body)
     try {
-        result = await postRegisterUserService(email, password)
+        result = await postRegisterUserService(email, password, fname, lname)
         if (result == 1) res.sendStatus(200)
         else res.sendStatus(500)
         // });
