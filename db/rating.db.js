ee92531d00d20fc6a7209089334cc28c0c1f8263
Linnea Dierksheide <ldierksheide@gwu.edu>
2022-03-02 17:53:30 -0500

    fix (but still issues) num stars update query for rating

--- db/rating.db.js
+++ db/rating.db.js
@@ -1,5 +1,6 @@
 const config = require('../config')
 const mysql = require("mysql");
+const util = require("util")
 
 
 var con = mysql.createConnection({
@@ -9,29 +10,28 @@ var con = mysql.createConnection({
     database: config.db.database,
 });
 
-const ratingDb = (bar_id, stars) => {
+const query = util.promisify(con.query).bind(con);
+
+const ratingDb = async (bar_id, stars) => {
     //TODO: Should probably check that the bar id is valid
 
-    var query = "SELECT avg_stars, review_count FROM bar WHERE bar_id = \"" + bar_id + "\";"
-    con.query(query, function (err, result) {
-        // TODO: Better error responses
-        if (err) throw err;
+    var query_txt = "SELECT avg_stars, review_count FROM bar WHERE bar_id = \"" + bar_id + "\";"
+    console.log(query_txt)
+    result = await query(query_txt);
+    console.log(result)
 
-        var rows = JSON.parse(JSON.stringify(result[0]));
+    var rows = JSON.parse(JSON.stringify(result[0]));
+    console.log(rows)
 
-        new_review_count = rows.review_count + 1
-        console.log(rows.avg_stars)
-        console.log(stars)
-        console.log(new_review_count)
-        new_avg_stars = (rows.avg_stars + stars) / new_review_count
-        console.log(new_avg_stars)
+    new_review_count = rows.review_count + 1
+    console.log(rows.avg_stars)
+    console.log(stars)
+    console.log(new_review_count)
+    new_avg_stars = (rows.avg_stars + stars)*1.0 / new_review_count
+    console.log("new average stars", new_avg_stars)
         
-        query = "UPDATE bar SET avg_stars = " + new_avg_stars + ", review_count = " + new_review_count + " WHERE bar_id = \"" + bar_id + "\";"
-        con.query(query, function (err, result) {
-            // TODO: Better error responses
-            if (err) throw err;
-        })
-    });
+    query_txt2 = "UPDATE bar SET avg_stars = " + new_avg_stars + ", review_count = " + new_review_count + " WHERE bar_id = \"" + bar_id + "\";"
+    result2 = query(query_txt2);
 }
 
 module.exports = {
