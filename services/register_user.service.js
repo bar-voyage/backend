10f14c32ef05dd0530150654c82e9e7bfa69f0f1
Linnea Dierksheide <ldierksheide@gwu.edu>
2022-03-07 15:52:54 -0500

    add age and gender to register account

--- services/register_user.service.js
+++ services/register_user.service.js
@@ -1,9 +1,9 @@
 const { registerUserDb } = require('../db/register_user.db')
 const { userLoginDb } = require('../db/user_login.db')
 
-const postRegisterUserService = async (email, password, fname, lname) => {
+const postRegisterUserService = async (email, password, fname, lname, age, gender) => {
     try {
-        return await registerUserDb(email, password, fname, lname)
+        return await registerUserDb(email, password, fname, lname, age, gender)
     } catch (e) {
         throw new Error(e.message)
     }
