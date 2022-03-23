f2b072f792cf7846c26a5c80caae24473d7104a5
Melody Lee <melodylee299@gmail.com>
2022-03-23 05:04:10 -0400

    Include bar name in response

--- db/get_curr_bar.db.js
+++ db/get_curr_bar.db.js
@@ -13,10 +13,10 @@ const query = util.promisify(con.query).bind(con);
 
 const getCurrBarDb = async (user_id) => {
     /* only one current bar available, so if there's already a current bar set for thie user, need to remove it before updating */
-    var query_txt = "SELECT bar_id FROM bar_history WHERE current_bar = 1 AND user_id = " + user_id + ";";
-    var rows = await query(query_txt);
-    console.log(rows[0].bar_id)
-    return rows[0].bar_id
+    var query_txt = "SELECT bar.bar_id, bar.name FROM (users JOIN bar_history on users.user_id = bar_history.user_id JOIN bar ON bar_history.bar_id = bar.bar_id) WHERE bar_history.current_bar = 1 AND users.user_id = " + user_id + ";";
+    var rows = await query(query_txt)
+    console.log("getCurrBarDb", rows)
+    return rows
 }
 
 module.exports = {
--- services/get_curr_bar.service.js
+++ services/get_curr_bar.service.js
@@ -2,9 +2,10 @@ const { getCurrBarDb } = require('../db/get_curr_bar.db')
 
 const getCurrBarService = async (user_id) => {
     try {        
-        const bar_id_value = await getCurrBarDb(user_id)
-        const json = "{\"curr_bar\" : " + bar_id_value + "}";
-        return json
+        // const bar_id_value = await getCurrBarDb(user_id)
+        // const json = "{\"curr_bar\" : " + bar_id_value + "}";
+        // return json
+        return await getCurrBarDb(user_id)
         
     } catch (e) {
         throw new Error(e.message)
